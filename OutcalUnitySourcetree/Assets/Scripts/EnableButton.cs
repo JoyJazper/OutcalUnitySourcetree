a0ac82309ebafdc4079dfebea3cae5dc34d86c66
JoyJazper <pjk096@gmail.com>
2020-06-12 22:43:00 +0530

    Updated Scene for the 2nd assignment. Awake,Start,Update,Ondestroy function used. Also Worked on UI buttons and Input Event Handling.

---
+++ OutcalUnitySourcetree/Assets/Scripts/EnableButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableButton : MonoBehaviour
+{
+    public GameObject awakeOnly_Object;
+
+    public void EnableScript()
+    {
+        awakeOnly_Object.GetComponent<JustAwake>().enabled = true;
+    }
+
+    public void DestroyScript()
+    {
+        Destroy(awakeOnly_Object);
+    }
+}
