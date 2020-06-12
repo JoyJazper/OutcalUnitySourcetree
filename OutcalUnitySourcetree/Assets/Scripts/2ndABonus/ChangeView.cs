1a376db433958a2fc87d57274bc042f2f888da0d
JoyJazper <pjk096@gmail.com>
2020-06-13 02:00:14 +0530

    Bonus assignment, multiple scene changing implemented.

---
+++ OutcalUnitySourcetree/Assets/Scripts/2ndABonus/ChangeView.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeView : MonoBehaviour
+{
+    public void ChangeScene(int scene_Number)
+    {
+        SceneManager.LoadScene(scene_Number);
+    }
+}
