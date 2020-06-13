bf8a9064e79cb3a39f5b510e200e2eb018785ad3
JoyJazper <pjk096@gmail.com>
2020-06-14 03:27:59 +0530

    Scene changing scripts implemented, code improvised.

---
+++ Assets/Scripts/SceneChange/ChangeToScene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace SceneChanger
+{
+    class ChangeToScene : MonoBehaviour
+    {
+        public Button toScene1;
+        public Button toScene2;
+        public Button toScene3;
+
+        void Start()
+        {
+            toScene1.onClick.AddListener(() => ChangeScene(1));
+            toScene2.onClick.AddListener(() => ChangeScene(2));
+            toScene3.onClick.AddListener(() => ChangeScene(3));
+        }
+
+        void ChangeScene(int scene_Number)
+        {
+            SceneManager.LoadScene(scene_Number);
+        }
+    }
+}
