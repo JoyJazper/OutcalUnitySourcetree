6e0de48c9d40d0f8a96974f0562dc6011140cbf5
JoyJazper <pjk096@gmail.com>
2020-06-14 18:18:05 +0530

    Code smells reduced.

---
+++ Assets/Scripts/ScriptCycleDemo/ScriptControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace ScriptHandler
+{
+    [RequireComponent(typeof(ScriptImplementDemo))]
+    public class ScriptControl:MonoBehaviour
+    {
+        ScriptImplementDemo scriptBase;
+
+        void Start()
+        {
+            scriptBase = this.GetComponent<ScriptImplementDemo>();
+        }
+
+        public void EnableScript()
+        {
+            scriptBase.enabled = true;
+        }
+
+        public void DestroyScript()
+        {
+            Debug.Log("Ran this.");
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptCycleDemo/ScriptImplementDemo.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ScriptHandler
+{
+    public class ScriptImplementDemo : MonoBehaviour
+    {
+        public Text awake_Notifier;
+        public Text start_Notifier;
+        public Text event_Notifier;   
+
+        void Awake()
+        {
+            awake_Notifier.text = this.name + "Object is Initialized. But script is not.";
+        }
+        
+        void Start()
+        {
+            start_Notifier.text = "Script is initialized, now you can press f.";
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F) || Input.GetMouseButtonDown(0))
+            {
+                event_Notifier.text = "Either F or Left Mouse button is pressed";
+            }
+
+            /*if (Input.GetKey(KeyCode.F))
+            {
+                Debug.Log("F is pressed.");
+            }*/
+
+            if (Input.GetKeyUp(KeyCode.F))
+            {
+                event_Notifier.text = "Either F is released";
+            }
+        }
+
+        void OnDestroy()
+        {
+            awake_Notifier.text = this.name + "Object is destroyed, Please restart";
+            start_Notifier.text = "";
+            event_Notifier.text = "";
+        }
+    }
+}
